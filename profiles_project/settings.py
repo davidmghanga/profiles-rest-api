3d2d913fc92f760208f66dcc79d0d49588dae71a
David Mghanga <damghanga@gmail.com>
2024-06-01 19:13:59 +0300

    Added allowed hosts.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'e8k%=)^7$!j3z_6&_!mldm(43#o5_&srp9az_#jbste=2al-kr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-13-51-177-123.eu-north-1.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
